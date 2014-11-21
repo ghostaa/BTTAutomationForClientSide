e787b2272a2a06a0b3779334b46b6fb900507bd0
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2014-11-21 06:27:12 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@21 0d26679e-a245-7d45-8790-df05fb349fae

--- BTTAutomation/src/btt/allwidgets/Label/ECA.java
+++ BTTAutomation/src/btt/allwidgets/Label/ECA.java
@@ -16,8 +16,10 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
+import bsh.util.Util;
 import btt.allwidgets.Index_Page;
 import btt.allwidgets.Main_Page;
+import btt.Util.*;
 
 import com.gargoylesoftware.htmlunit.javascript.host.Element;
 import com.opera.core.systems.internal.input.KeyEvent;
@@ -28,7 +30,7 @@ import org.openqa.selenium.remote.server.handler.interactions.MouseUp;
 import java.util.*;
 
 public class ECA {
-  private static WebDriver driver;
+  public static WebDriver driver;
   private static String baseUrl;
   private boolean acceptNextAlert = true;
   private static StringBuffer verificationErrors = new StringBuffer();
@@ -37,32 +39,114 @@ public class ECA {
   private static Label_PageObject drv;
   
   @BeforeClass
-  public static void setUp() throws Exception {	  
+  public static void setUp() throws Exception {
 	driver = new FirefoxDriver();
-	String baseUrl = "http://localhost:8080/";
+	baseUrl = "http://localhost:8080/InternetBankTestWeb/";
 	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	driver.get(baseUrl + "InternetBankTestWeb/");
+	driver.get(baseUrl);
 	drv=PageFactory.initElements(driver, Label_PageObject.class);
 	drv.Establish();
     drv.BTT8200_tab();
     drv.Label_widget();
     drv.label_ECA(); 
-    drv.ID_exp();
-    drv.Text_exp();
-    drv.ID_false();
-    drv.ID_visibility();
   }
 
+//###############################For Label Event######################################  
+  @Test
+  public void Evt_onclick(){
+	  drv.Evt_onclick();
+	  assertEquals("click ok", drv.Evt_onclick_getText());
+	  By.className("");
+  }
+  
+  @Test
+  public void Evt_onMouseup(){
+	  drv.Evt_onMouseup();
+	  assertEquals("'onMouseup ok'", drv.Evt_onMouseup_getText());
+  }
+  
+  @Test
+  public void Evt_onMouseLeave(){
+	  drv.Evt_onMouseLeave();
+	  assertEquals("'onMouseLeave ok'", drv.Evt_onMouseLeave_getText());
+  }
+  
+  @Test
+  public void Evt_onMouseMove(){
+	  drv.Evt_onMouseMove();
+	  assertEquals("onMouseMove ok", drv.Evt_onMouseMove_getText());
+  }
+  
+  @Test
+  public void Evt_onMouseEnter(){
+	  drv.Evt_onMouseEnter();
+	  assertEquals("onMouseEnter ok", drv.Evt_onMouseEnter_getText());
+  }
+  
+  @Test
+  public void Evt_onMousedown(){
+	  drv.Evt_onMousedown();
+	  assertEquals("onMouseDown ok", drv.Evt_onMousedown_getText());
+  }
+//###############################For Label Event######################################
+//###############################For Label Condition##################################
   @Test
   public void ID_exp(){
-	  assertEquals("ok", drv.ID_exp_getText());
-  }
-  @Test
-  public void ID_exp1(){
+	  drv.ID_exp();
 	  assertEquals("ok", drv.ID_exp_getText());
   }
   
-
+  @Test
+  public void Text_exp(){
+	  drv.Text_exp();
+	  assertEquals("ok",drv.Text_exp_getText());
+  }
+  
+  @Test
+  public void ID_false(){
+	  drv.ID_false();
+	  assertEquals("false",drv.ID_false_getText());
+  }
+  
+  @Test
+  public void ID_visibility(){
+	  drv.ID_visibility();
+	  assertEquals("ok",drv.ID_visibility_getText());
+  }
+//###############################For Label Condition##################################
+//###############################For Label Action#####################################
+  @Test
+  public void label_action_hidden(){
+	  drv.label_action_hiddenClick();
+	  assertEquals(false, drv.label_action_hidden());
+  }
+  
+  @Test
+  public void label_action_gone(){
+	  drv.label_action_goneClick();
+	  assertEquals(false, drv.label_action_gone());
+  }
+  
+  @Test
+  public void label_action_show(){
+	  drv.label_action_showClick();
+	  assertEquals(true, drv.label_action_show());
+  }
+  
+  @Test
+  public void label_action_change(){
+	  drv.label_action_changeClick();
+	  assertEquals("text ok", drv.label_action_change());
+  }
+  
+  @Test
+  public void css_check() throws InterruptedException{
+	  drv.Label_Evt_Click();
+	  String[] temp={"pointer_down"};
+	  assertEquals(1,CSS.css_query(drv.css_getAttribute(), temp));
+  }
+  
+//###############################For Label Action#####################################
   @AfterClass
   public static void tearDown() throws Exception {
     driver.quit();
