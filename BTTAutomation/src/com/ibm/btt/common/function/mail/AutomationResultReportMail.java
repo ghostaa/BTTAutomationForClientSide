6194d912fd3e1e14408ad43f07fd3e7d74a60c4f
ydp <ydp@0d26679e-a245-7d45-8790-df05fb349fae>
2014-11-24 07:29:40 +0000

    加入email功能，并可以通过配置文件MailConfig.xml可以配置
    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@26 0d26679e-a245-7d45-8790-df05fb349fae

---
+++ BTTAutomation/src/com/ibm/btt/common/function/mail/AutomationResultReportMail.java
@@ -0,0 +1,361 @@
+package com.ibm.btt.common.function.mail;
+
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.Address;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import mx4j.log.Log4JLogger;
+
+public class AutomationResultReportMail {
+	private MimeMessage mimeMsg; // MIME邮件对象
+	private Session session; // 邮件会话对象
+	private Properties props; // 系统属性
+	private boolean needAuth = false; // smtp是否需要认证
+	private String username = ""; // smtp认证用户名和密码
+	private String password = "";
+	private Multipart mp; // Multipart对象,邮件内容,标题,附件等内容均添加到其中后再生成//MimeMessage对象
+	private MailConfigAdapter config= new MailConfigAdapter();
+	
+	
+	public AutomationResultReportMail() {
+		
+	}
+	public AutomationResultReportMail(String smtp) {
+		setSmtpHost(smtp);
+		createMimeMessage();
+
+	}
+
+	public void setSmtpHost(String hostName){
+
+		System.out.println("设置系统属性：mail.smtp.host = " + hostName);
+
+		if (props == null)
+			props = System.getProperties(); // 获得系统属性对象
+			props.put("mail.smtp.host", hostName); // 设置SMTP主机
+			// props.put("mail.smtp.port", 465); // 设置SMTP主机
+
+	}
+
+
+	public boolean createMimeMessage() {
+
+		try {
+
+			System.out.println("准备获取邮件会话对象！");
+
+			session = Session.getDefaultInstance(props, null); // 获得邮件会话对象
+
+		}
+
+		catch (Exception e){
+
+			System.err.println("获取邮件会话对象时发生错误！" + e);
+
+			return false;
+
+		}
+
+		System.out.println("准备创建MIME邮件对象！");
+
+		try {
+
+			mimeMsg = new MimeMessage(session); // 创建MIME邮件对象
+
+			mp = new MimeMultipart(); // mp 一个multipart对象
+
+			// Multipart is a container that holds multiple body parts.
+
+			return true;
+
+		}
+
+		catch (Exception e){
+
+			System.err.println("创建MIME邮件对象失败！" + e);
+
+			return false;
+
+		}
+
+	}
+
+
+
+	public void setNeedAuth(boolean need) {
+
+		System.out.println("设置smtp身份认证：mail.smtp.auth = " + need);
+
+		if (props == null)
+
+			props = System.getProperties();
+
+		if (need) {
+
+			props.put("mail.smtp.auth", "true");
+
+		} else {
+
+			props.put("mail.smtp.auth", "false");
+
+		}
+
+	}
+
+
+	public void setNamePass(String name, String pass){
+
+		System.out.println("程序得到用户名与密码");
+
+		username = name;
+
+		password = pass;
+
+	}
+
+
+	public boolean setSubject(String mailSubject) {
+
+		System.out.println("设置邮件主题！");
+
+		try {
+
+			mimeMsg.setSubject(mailSubject);
+
+			return true;
+
+		}catch (Exception e) {
+
+			System.err.println("设置邮件主题发生错误！");
+
+			return false;
+
+		}
+
+	}
+	public boolean setBody(String mailBody){
+
+		try{
+
+			System.out.println("设置邮件体格式");
+
+			BodyPart bp = new MimeBodyPart();
+
+			bp.setContent(
+
+			"<meta http-equiv=Content-Type content=text/html; charset=gb2312>"
+
+			+ mailBody, "text/html;charset=GB2312");
+
+			mp.addBodyPart(bp);
+
+			return true;
+
+		}
+
+		catch (Exception e)
+
+		{
+
+			System.err.println("设置邮件正文时发生错误！" + e);
+
+			return false;
+
+		}
+
+	}
+
+
+	public boolean addFileAffix(String filename) {
+
+		System.out.println("增加邮件附件：" + filename);
+
+		try {
+
+			BodyPart bp = new MimeBodyPart();
+
+			FileDataSource fileds = new FileDataSource(filename);
+
+			bp.setDataHandler(new DataHandler(fileds));
+
+			bp.setFileName(fileds.getName());
+
+			mp.addBodyPart(bp);
+
+			return true;
+
+		}
+
+		catch (Exception e) {
+
+			System.err.println("增加邮件附件：" + filename + "发生错误！" + e);
+
+			return false;
+
+		}
+
+	}
+
+
+	public boolean setFrom(String from) {
+
+		System.out.println("设置发信人！");
+
+		try {
+
+			mimeMsg.setFrom(new InternetAddress(from)); // 设置发信人
+
+			return true;
+
+		}
+
+		catch (Exception e){
+
+			return false;
+
+		}
+
+	}
+
+	public boolean setTo(String to){
+
+		System.out.println("设置收信人");
+
+		if (to == null)
+
+			return false;
+
+		try{
+
+			/*
+			 * mimeMsg.setRecipients(Message.RecipientType.TO, InternetAddress
+			 * 
+			 * .parse(to));
+			 */
+
+//			String toList = "xiulsun@cn.ibm.com,dpyan@cn.ibm.com,vndliyj@cn.ibm.com,zhaomm@cn.ibm.com,jshang@cn.ibm.com";
+//			InternetAddress[] iaToList = new InternetAddress().parse(toList);
+			InternetAddress[] iaToList = new InternetAddress().parse(to);
+
+			mimeMsg.setRecipients(Message.RecipientType.TO, iaToList);
+
+			return true;
+
+		}catch (Exception e){
+
+			return false;
+
+		}
+
+	}
+
+	public boolean setCopyTo(String copyto){
+
+		System.out.println("发送附件到");
+
+		if (copyto == null)
+
+			return false;
+
+		try {
+
+			mimeMsg.setRecipients(Message.RecipientType.CC,
+
+			(Address[]) InternetAddress.parse(copyto));
+
+			return true;
+
+		}catch (Exception e){
+			return false;
+		}
+
+	}
+
+	public boolean sendout(){
+
+		try{
+
+			mimeMsg.setContent(mp);
+
+			mimeMsg.saveChanges();
+
+			System.out.println("正在发送邮件....");
+
+			Session mailSession = Session.getInstance(props, null);
+
+			mailSession.setDebug(true);
+
+			Transport transport = mailSession.getTransport("smtp");
+
+			transport.connect((String) props.get("mail.smtp.host"), username,
+
+			password);
+
+			transport.sendMessage(mimeMsg, mimeMsg
+
+			.getRecipients(Message.RecipientType.TO));
+
+			// transport.send(mimeMsg);
+
+			System.out.println("发送邮件成功！");
+
+			transport.close();
+
+			return true;
+
+		}catch (Exception e){
+			System.err.println("邮件发送失败！" + e);
+			return false;
+
+		}
+
+	}
+
+	public void sendMailToAllTester(String mailBody){
+
+		AutomationResultReportMail themail = new AutomationResultReportMail(
+				config.getSMTPHomeName());
+
+		themail.setNeedAuth(true);
+
+		if (themail.setSubject(config.getSubject()) == false)
+			return;
+
+		if (themail.setBody(mailBody) == false)
+			return;
+
+		// 收件人邮箱
+		if (themail.setTo(config.getAllTester()) == false)
+			return;
+
+		// 发件人邮箱
+		if (themail.setFrom(config.getSendFrom()) == false)
+			return;
+
+		// 设置附件
+		// if (themail.addFileAffix("#######") == false)
+		// return; // 附件在本地机子上的绝对路径
+		themail.setNamePass("dpyan@cn.ibm.com", "11"); // 用户名与密码
+		if (themail.sendout() == false)
+			return;
+
+	}
+	
+	public static void main(String[] args){
+
+		String mailBody = " <font color=red>BTT 自动化测试结果报告</font> <a href=\"#\">请点击</a>";
+		new AutomationResultReportMail().sendMailToAllTester(mailBody);
+
+	}
+}
---
+++ BTTAutomation/src/com/ibm/btt/common/function/mail/MailConfigAdapter.java
@@ -0,0 +1,112 @@
+package com.ibm.btt.common.function.mail;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.DOMReader;
+import org.dom4j.io.SAXReader;
+public class MailConfigAdapter {
+	private Document doc;
+	private SAXReader saxReader;
+	private Element root;
+	
+	public MailConfigAdapter(){
+		saxReader = new SAXReader();
+		try {
+			 doc = saxReader.read(new File("./src/MailConfig.xml"));
+			 root = doc.getRootElement();
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+			
+	}
+	/**
+	 * 
+	 * @return 从配置文件中获得SMTP地址
+	 */
+	public String getSMTPHomeName(){
+		Element SMTPHomeName = root.element("MailService").element("SMTPHomeName");
+		return SMTPHomeName.getText();
+	}
+	/**
+	 * 
+	 * @return 从配置文件中获得邮件的标题
+	 */
+	public String getSubject(){
+		Element subject = root.element("MailService").element("Subject");
+		return subject.getText();
+	}
+	/**
+	 * 
+	 * @return 从配置文件中获得所有测试人员的列表
+	 */
+	public String getAllTester(){
+		List allTesterList= root.element("User").element("UserGroup").element("AllTester").elements();
+		StringBuffer sb =new StringBuffer();
+		for(Iterator iter = allTesterList.iterator(); iter.hasNext();){
+			Element e = (Element)iter.next();
+			
+			System.out.println(e.getText());
+			sb.append(e.getText()+",");
+		}
+		
+		return sb.toString();
+	}
+	
+	public String getSendFrom(){
+		Element sendFrom =  root.element("User").element("SendFrom");
+		return sendFrom.getText();
+	}
+	
+	
+	public static void main(String[] args) throws Exception{
+			SAXReader saxReader = new SAXReader();
+			
+			Document doc = saxReader.read(new File("./src/MailConfig.xml"));
+			
+			Element root = doc.getRootElement();
+			
+			System.out.println("root element: " + root.getName());
+			
+			List childList = root.elements();
+			
+			System.out.println(childList.size());
+			
+			MailConfigAdapter mca = new MailConfigAdapter();
+			System.out.println(mca.getSMTPHomeName());
+			System.out.println(mca.getSubject());
+			System.out.println(mca.getAllTester());
+			System.out.println(mca.getSendFrom());
+			/*
+			for(Iterator iter = root.elementIterator(); iter.hasNext();)
+			{
+				Element e = (Element)iter.next();
+				
+				System.out.println(e.attributeValue("age"));
+			}
+			
+			System.out.println("---------------------------");
+			
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			org.w3c.dom.Document document = db.parse(new File("student2.xml"));
+			
+			DOMReader domReader = new DOMReader();
+			
+			//将JAXP的Document转换为dom4j的Document
+			Document d = domReader.read(document);
+			
+			Element rootElement = d.getRootElement();
+			
+			System.out.println(rootElement.getName());*/
+
+	}
+
+}
