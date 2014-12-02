bbf3e7561417f667b96cc7e6917b107c80b7a173
zmm <zmm@0d26679e-a245-7d45-8790-df05fb349fae>
2014-12-02 03:10:22 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@36 0d26679e-a245-7d45-8790-df05fb349fae

--- BTTAutomation/src/com/ibm/btt/util/Tools.java
+++ BTTAutomation/src/com/ibm/btt/util/Tools.java
@@ -1,13 +1,40 @@
 package com.ibm.btt.util;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+
+
 public class Tools {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static void snapshot(TakesScreenshot drivername, String filename)
+	  {
+	      // this method will take screen shot ,require two parameters ,one is driver name, another is file name
+	      
+	    String currentPath = System.getProperty("user.dir"); //get current work folder
+	    System.out.println(currentPath);
+	    File scrFile = drivername.getScreenshotAs(OutputType.FILE);
+	        // Now you can do whatever you need to do with it, for example copy somewhere
+	        try {
+	            System.out.println("save snapshot path is:"+currentPath+"/"+filename);
+	            FileUtils.copyFile(scrFile, new File(currentPath+"\\"+filename));
+	        } catch (IOException e) {
+	            // TODO Auto-generated catch block
+	            System.out.println("Can't save screenshot");
+	            e.printStackTrace();
+	        } 
+	        finally
+	        {
+	           
+	            System.out.println("screen shot finished");
+	        }
+	  }
 
 }
