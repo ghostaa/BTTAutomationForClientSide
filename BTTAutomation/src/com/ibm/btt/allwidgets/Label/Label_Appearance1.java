85bb5cd1a101248e59f6abc381e3bfcf82bf95fb
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2015-01-16 08:00:46 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@75 0d26679e-a245-7d45-8790-df05fb349fae

---
+++ BTTAutomation/src/com/ibm/btt/allwidgets/Label/Label_Appearance1.java
@@ -0,0 +1,100 @@
+package com.ibm.btt.allwidgets.Label;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import com.ibm.btt.util.DateType;
+import com.ibm.btt.util.Main_Class;
+import com.ibm.btt.util.NLS;
+import com.ibm.btt.util.PropertiesUtil;
+import com.ibm.btt.util.Tools;
+import com.ibm.btt.allwidgets.Text.Text_Condition;
+
+public class Label_Appearance1 extends Main_Class{
+  /*private static WebDriver driver;
+  private static String baseUrl = PropertiesUtil.baseUrl;*/
+  private boolean acceptNextAlert = true;
+  private static StringBuffer verificationErrors = new StringBuffer();
+  private String result_txt="Passed";
+  protected java.util.List<String> checkPoint=new ArrayList<String>();
+  private static Label_PageObject drv;
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+	/*driver = new FirefoxDriver();
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	driver.get(baseUrl);*/
+	Main_Class.setUp();
+	drv=PageFactory.initElements(driver, Label_PageObject.class);
+	drv.Establish();
+    drv.BTT8200_tab();
+    drv.Label_widget();
+    drv.label_appearance();
+  }
+
+  @Test
+  public void appearance1_1(){
+//	  assertEquals("top+left", drv.appearance1_1());
+	  drv.appearance1_1();
+  }
+
+  
+  @AfterClass
+  public static void tearDown() throws Exception {
+	Tools.snapshot((TakesScreenshot)driver, Main_Class.allwidgets,Label_Appearance1.class);
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
