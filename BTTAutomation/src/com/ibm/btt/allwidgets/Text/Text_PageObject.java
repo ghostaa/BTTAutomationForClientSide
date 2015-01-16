b61fa2f06809e43ecb3a0fe551e0ed82764843e8
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2015-01-16 08:02:31 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@81 0d26679e-a245-7d45-8790-df05fb349fae

--- BTTAutomation/src/com/ibm/btt/allwidgets/Text/Text_PageObject.java
+++ BTTAutomation/src/com/ibm/btt/allwidgets/Text/Text_PageObject.java
@@ -613,5 +613,132 @@ public class Text_PageObject extends Main_Page{
 		return Text_Action_getHint.getAttribute("title");
 	}
 //###############################Text Action######################################
+//###########################Text Appearance1#################################
+	@FindBy(id="widget_Text_appearance1_text")
+	private WebElement Appearance1_1;
+	
+	public String Appearance1_1(){//1.Width is AutoSize,Height is AutoSize
+		return Appearance1_1.getAttribute("style");
+	}
+	
+	@FindBy(id="widget_Text_appearance1_text01")
+	private WebElement Appearance1_2;
+	
+	public String Appearance1_2(){//2.Width is Fixed 200px,Height is AutoSize
+		return Appearance1_2.getAttribute("style");
+	}
+	
+	@FindBy(id="widget_Text_appearance1_text02")
+	private WebElement Appearance1_3;
+	
+	public String Appearance1_3(){//3.Width is Fill,Height is AutoSize
+		return Appearance1_3.getAttribute("style");
+	}
+	
+	@FindBy(id="widget_Text_appearance1_text03")
+	private WebElement Appearance1_4;
+	
+	public String Appearance1_4(){//4.Width is Relative 30%,Height is AutoSize
+		return Appearance1_4.getAttribute("style");
+	}
+//###############################Text Appearance1###############################
+//###############################Text Appearance2###############################
+	@FindBy(xpath=".//*[@id='Text_appearance2_group']/div/div/div/div")
+	private WebElement Appearance2_1;
+	
+	public String Appearance2_1(){//1.horizontal is left,vertical is top
+		String v = Appearance2_1.getAttribute("style");
+		String h = Appearance2_1.getAttribute("align");
+		return h+"&"+v;
+	}
+	
+	@FindBy(xpath=".//*[@id='Text_appearance2_group01']/div/div/div/div")
+	private WebElement Appearance2_2;
+	
+	public String Appearance2_2(){//2.horizontal is left,vertical is center
+		String v = Appearance2_2.getAttribute("style");
+		String h = Appearance2_2.getAttribute("align");
+		return h+"&"+v;
+	}
+	
+	@FindBy(xpath=".//*[@id='Text_appearance2_group02']/div/div/div/div")
+	private WebElement Appearance2_3;
+	
+	public String Appearance2_3(){//3.horizontal is left,vertical is bottom
+		String v = Appearance2_3.getAttribute("style");
+		String h = Appearance2_3.getAttribute("align");
+		return h+"&"+v;
+	}
+	
+	@FindBy(xpath=".//*[@id='Text_appearance2_group03']/div/div/div/div")
+	private WebElement Appearance2_4;
+	
+	public String Appearance2_4(){//4.horizontal is center,vertical is top
+		String v = Appearance2_4.getAttribute("style");
+		String h = Appearance2_4.getAttribute("align");
+		return h+"&"+v;
+	}
+	
+	@FindBy(xpath=".//*[@id='Text_appearance2_group04']/div/div/div/div")
+	private WebElement Appearance2_5;
+	
+	public String Appearance2_5(){//5.horizontal is center,vertical is center
+		String v = Appearance2_5.getAttribute("style");
+		String h = Appearance2_5.getAttribute("align");
+		return h+"&"+v;
+	}
+	
+	@FindBy(xpath=".//*[@id='Text_appearance2_group05']/div/div/div/div")
+	private WebElement Appearance2_6;
+	
+	public String Appearance2_6(){//6.horizontal is center,vertical is bottom
+		String v = Appearance2_6.getAttribute("style");
+		String h = Appearance2_6.getAttribute("align");
+		return h+"&"+v;
+	}
+	
+	@FindBy(xpath=".//*[@id='Text_appearance2_group06']/div/div/div/div")
+	private WebElement Appearance2_7;
+	
+	public String Appearance2_7(){//7.horizontal is right,vertical is top
+		String v = Appearance2_7.getAttribute("style");
+		String h = Appearance2_7.getAttribute("align");
+		return h+"&"+v;
+	}
+	
+	@FindBy(xpath=".//*[@id='Text_appearance2_group07']/div/div/div/div")
+	private WebElement Appearance2_8;
+	
+	public String Appearance2_8(){//8.horizontal is right,vertical is center
+		String v = Appearance2_8.getAttribute("style");
+		String h = Appearance2_8.getAttribute("align");
+		return h+"&"+v;
+	}
+	
+	@FindBy(xpath=".//*[@id='Text_appearance2_group08']/div/div/div/div")
+	private WebElement Appearance2_9;
+	
+	public String Appearance2_9(){//9.horizontal is right,vertical is bottom
+		String v = Appearance2_9.getAttribute("style");
+		String h = Appearance2_9.getAttribute("align");
+		return h+"&"+v;
+	}
+	
+	@FindBy(xpath=".//*[@id='Text_appearance2_group09']/div/div/div/div")
+	private WebElement Appearance2_10;
+	
+	public String Appearance2_10(){//10.horizontal indent is 50px
+		return Appearance2_10.getAttribute("style");
+		
+	}
+	
+	@FindBy(xpath=".//*[@id='Text_appearance2_group10']/div/div/div/div")
+	private WebElement Appearance2_11;
+	
+	public String Appearance2_11(){//11.horizontal indent is 10percent
+		return Appearance2_11.getAttribute("style");
+	}
+//###############################Text Appearance2###############################
+
 
 }
