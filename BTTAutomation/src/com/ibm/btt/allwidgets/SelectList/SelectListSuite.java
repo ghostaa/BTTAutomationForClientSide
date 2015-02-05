264ea265ec389dfcebe7eec2158a35e04597a6ad
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2015-02-05 07:14:21 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@113 0d26679e-a245-7d45-8790-df05fb349fae

---
+++ BTTAutomation/src/com/ibm/btt/allwidgets/SelectList/SelectListSuite.java
@@ -0,0 +1,18 @@
+package com.ibm.btt.allwidgets.SelectList;
+import org.junit.runner.RunWith;  
+import org.junit.runners.Suite;   
+  
+@RunWith(Suite.class)  
+@Suite.SuiteClasses({    
+	SelectList_Properties.class,    
+	SelectList_Event.class,
+	SelectList_Condition.class,
+	SelectList_Action.class,
+	SelectList_CSS.class,
+	SelectList_appearance.class,
+//	SelectList_Submit.class,
+	
+}) 
+public class SelectListSuite {
+
+}
