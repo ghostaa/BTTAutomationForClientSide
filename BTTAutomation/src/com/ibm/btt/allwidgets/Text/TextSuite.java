3090a2e6b0b3024f9ffc0e8a5b4ca2f8c154e4d6
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2015-02-05 07:08:35 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@106 0d26679e-a245-7d45-8790-df05fb349fae

---
+++ BTTAutomation/src/com/ibm/btt/allwidgets/Text/TextSuite.java
@@ -0,0 +1,18 @@
+package com.ibm.btt.allwidgets.Text;
+import org.junit.runner.RunWith;  
+import org.junit.runners.Suite;   
+  
+@RunWith(Suite.class)  
+@Suite.SuiteClasses({    
+	Text_Properties.class,    
+	Text_Event.class,
+	Text_Condition.class,
+	Text_Action.class,
+	Text_CSS.class,
+	Text_appearance1.class,
+	Text_appearance2.class,
+	
+}) 
+public class TextSuite {
+
+}
