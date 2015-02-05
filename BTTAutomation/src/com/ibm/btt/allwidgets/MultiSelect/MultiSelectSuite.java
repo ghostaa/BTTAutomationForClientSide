bf01fa05fee34c8f7839f595a99ff472a65f461c
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2015-02-05 07:19:03 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@114 0d26679e-a245-7d45-8790-df05fb349fae

---
+++ BTTAutomation/src/com/ibm/btt/allwidgets/MultiSelect/MultiSelectSuite.java
@@ -0,0 +1,17 @@
+package com.ibm.btt.allwidgets.MultiSelect;
+import org.junit.runner.RunWith;  
+import org.junit.runners.Suite;   
+  
+@RunWith(Suite.class)  
+@Suite.SuiteClasses({    
+	MultiSelect_Properties.class,    
+	MultiSelect_ECA.class,
+	MultiSelect_CSS.class,
+//	MultiSelect_Submit.class,
+	MultiSelect_appearance1.class,
+	MultiSelect_appearance2.class,
+	
+}) 
+public class MultiSelectSuite {
+
+}
