572bddc41ef815dc66f487713618f22f3d3be4c6
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2015-02-05 07:06:42 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@105 0d26679e-a245-7d45-8790-df05fb349fae

---
+++ BTTAutomation/src/com/ibm/btt/allwidgets/Label/LabelSuite.java
@@ -0,0 +1,16 @@
+package com.ibm.btt.allwidgets.Label;
+import org.junit.runner.RunWith;  
+import org.junit.runners.Suite;   
+  
+@RunWith(Suite.class)  
+@Suite.SuiteClasses({    
+	Label_Properties.class,    
+	Label_ECA.class,
+	Label_Appearance.class,
+	Label_Appearance1.class,
+	Label_CSS.class,
+	
+}) 
+public class LabelSuite {
+
+}
