b485d3d751f8e838341f4ddf523a43f0c2947b83
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2015-02-05 07:12:32 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@111 0d26679e-a245-7d45-8790-df05fb349fae

---
+++ BTTAutomation/src/com/ibm/btt/allwidgets/Link/LinkSuite.java
@@ -0,0 +1,18 @@
+package com.ibm.btt.allwidgets.Link;
+import org.junit.runner.RunWith;  
+import org.junit.runners.Suite;   
+  
+@RunWith(Suite.class)  
+@Suite.SuiteClasses({    
+	Link_Properties.class,    
+	Link_Event.class,
+	Link_Condition.class,
+	Link_Action.class,
+	Link_CSS.class,
+	Link_appearance1.class,
+	Link_appearance2.class,
+	
+}) 
+public class LinkSuite {
+
+}
