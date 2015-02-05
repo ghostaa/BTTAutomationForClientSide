df5c66a9fea2db810701f8d2f3804435f0a8c918
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2015-02-05 07:24:36 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@116 0d26679e-a245-7d45-8790-df05fb349fae

---
+++ BTTAutomation/src/com/ibm/btt/allwidgets/VerticalSlider/VerticalSliderSuite.java
@@ -0,0 +1,18 @@
+package com.ibm.btt.allwidgets.VerticalSlider;
+import org.junit.runner.RunWith;  
+import org.junit.runners.Suite;   
+  
+@RunWith(Suite.class)  
+@Suite.SuiteClasses({    
+//	VerticalSlider_Properties.class,    
+	VerticalSlider_event.class,
+	VerticalSlider_condition.class,
+	VerticalSlider_action.class,
+	VerticalSlider_CSS.class,
+	VerticalSlider_appearance1.class,
+	VerticalSlider_appearance2.class,
+	
+}) 
+public class VerticalSliderSuite {
+
+}
