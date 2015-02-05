83f611839f9d799d60672e656d88652c300875e1
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2015-02-05 07:25:13 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@117 0d26679e-a245-7d45-8790-df05fb349fae

---
+++ BTTAutomation/src/com/ibm/btt/allwidgets/CheckedMultiSelect/CheckedMultiSelectSuite.java
@@ -0,0 +1,20 @@
+package com.ibm.btt.allwidgets.CheckedMultiSelect;
+import org.junit.runner.RunWith;  
+import org.junit.runners.Suite;   
+  
+@RunWith(Suite.class)  
+@Suite.SuiteClasses({    
+	CheckedMultiSelect_Properties.class,    
+	CheckedMultiSelect_event.class,
+	CheckedMultiSelect_condition.class,
+	CheckedMultiSelect_action.class,
+	CheckedMultiSelect_CSS.class,
+	CheckedMultiSelect_appearance1.class,
+	CheckedMultiSelect_appearance2.class,
+	CheckedMultiSelect_appearance3.class,
+//	CheckedMultiSelect_Submit.class,
+	
+}) 
+public class CheckedMultiSelectSuite {
+
+}
