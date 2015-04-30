987d013477e8c8273e80738d836d5828a2a864ee
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2015-04-30 09:12:09 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@142 0d26679e-a245-7d45-8790-df05fb349fae

--- BTTAutomation/src/com/ibm/btt/allwidgets/TextArea/TextAreaSuite.java
+++ BTTAutomation/src/com/ibm/btt/allwidgets/TextArea/TextAreaSuite.java
@@ -5,9 +5,9 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)  
 @Suite.SuiteClasses({    
 	TextArea_Properties.class,    
-//	TextArea_event.class,
-//	TextArea_condition.class,
-//	TextArea_action.class,
+	TextArea_event.class,
+	TextArea_condition.class,
+	TextArea_action.class,
 	TextArea_CSS.class,
 	TextArea_appearance1.class,
 	TextArea_appearance2.class,
