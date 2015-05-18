fa4d0cb16ed5e06f735b2e795749c71a4612d62f
lmm <lmm@0d26679e-a245-7d45-8790-df05fb349fae>
2015-05-18 09:24:28 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@184 0d26679e-a245-7d45-8790-df05fb349fae

---
+++ BTTAutomation/src/com/ibm/btt/allwidgets/FileUpload/FileUpload_Properties.java
@@ -0,0 +1,128 @@
+package com.ibm.btt.allwidgets.FileUpload;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import com.ibm.btt.util.Main_Class;
+import com.ibm.btt.util.PropertiesUtil;
+import com.ibm.btt.util.Tools;
+import com.ibm.btt.util.Event;
+
+public class FileUpload_Properties extends Main_Class {
+
+	private boolean acceptNextAlert = true;
+	private static StringBuffer verificationErrors = new StringBuffer();
+	private static FileUpload_PageObject drv;
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		
+		Main_Class.setUp();
+		drv = PageFactory.initElements(driver, FileUpload_PageObject.class);
+		drv.Establish();
+		drv.BTT8200_tab();
+		drv.FileUpload_widget();
+		drv.FileUpload_properties();
+
+	}
+	
+	@Test
+	public void proId(){
+		assertEquals("FileUpload_properties_myfileUpload", drv.proId());
+	}
+
+	@Test
+	public void proVisible() {
+		assertEquals(true, drv.proVisible());
+	}
+
+	@Test
+	public void proHidden() {
+		assertEquals(1, drv.proHidden());
+	}
+	
+	@Test
+	public void proGone(){
+		assertEquals(1, drv.proGone());
+	}
+	
+	@Test
+	public void proDisableT(){
+		assertEquals(1, drv.proDisableT());
+	}
+	
+	@Test
+	public void proDisableF(){
+		assertEquals(0, drv.proDisableF());
+	}
+	
+	@Test
+	public void proHint(){
+		assertEquals("Hello ?", drv.proHint());
+	}
+
+	@AfterClass
+	public static void tearDown() throws Exception {
+		Tools.snapshot((TakesScreenshot) driver, Main_Class.allwidgets,FileUpload_Properties.class);
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+}
