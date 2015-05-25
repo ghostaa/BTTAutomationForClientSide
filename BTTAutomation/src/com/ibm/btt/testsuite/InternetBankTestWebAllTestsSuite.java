06ff790a03500996b91e0ccefcbd5faf6ed99e4c
ydp <ydp@0d26679e-a245-7d45-8790-df05fb349fae>
2015-05-25 07:47:21 +0000

    Add Internet suite
    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@198 0d26679e-a245-7d45-8790-df05fb349fae

---
+++ BTTAutomation/src/com/ibm/btt/testsuite/InternetBankTestWebAllTestsSuite.java
@@ -0,0 +1,16 @@
+package com.ibm.btt.testsuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.ibm.btt.internetbanktestweb.fvt.pmr.p02698000852.NumberConversionInVietnamese;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	NumberConversionInVietnamese.class
+	
+})
+public class InternetBankTestWebAllTestsSuite {
+
+}
