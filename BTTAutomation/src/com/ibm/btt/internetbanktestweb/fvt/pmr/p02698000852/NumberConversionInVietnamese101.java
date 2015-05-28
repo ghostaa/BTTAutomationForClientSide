fb5bc83dc756b66f1a4ef73f1e5d985017a1bb03
sj <sj@0d26679e-a245-7d45-8790-df05fb349fae>
2015-05-28 08:19:34 +0000

    vi-text data auto test script
    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@210 0d26679e-a245-7d45-8790-df05fb349fae

--- BTTAutomation/src/com/ibm/btt/internetbanktestweb/fvt/pmr/p02698000852/NumberConversionInVietnamese101.java
+++ BTTAutomation/src/com/ibm/btt/internetbanktestweb/fvt/pmr/p02698000852/NumberConversionInVietnamese101.java
@@ -14,7 +14,7 @@ import com.ibm.btt.util.InternetBankTestWebMain_Class;
 import com.ibm.btt.util.Tools;
 
 //PMR 02698.000.852
-public class NumberConversionInVietnamese extends PMR02698000852_MainClass{
+public class NumberConversionInVietnamese101 extends PMR02698000852_MainClass{
 	private static PMR02698000852_PageObject drv;
 	private static StringBuffer verificationErrors = new StringBuffer();
 
@@ -39,12 +39,12 @@ public class NumberConversionInVietnamese extends PMR02698000852_MainClass{
 	@Test
 	public void TenTypes_Currency(){
 		drv.getAutoFill_button_101().click();
-		assertEquals("US$ 101.80",drv.getTenTypes_Text_Currency().getAttribute("value"));
+		assertEquals("102 VND",drv.getTenTypes_Text_Currency().getAttribute("value"));
 	}
 	@Test
 	public void TenTypes_Number(){
 		drv.getAutoFill_button_101().click();
-		assertEquals("101.8",drv.getTenTypes_Text_Number().getAttribute("value"));
+		assertEquals("101,8",drv.getTenTypes_Text_Number().getAttribute("value"));
 	}
 	@Test
 	public void TenTypes_Short(){
@@ -64,12 +64,12 @@ public class NumberConversionInVietnamese extends PMR02698000852_MainClass{
 	@Test
 	public void TenTypes_Float(){
 		drv.getAutoFill_button_101().click();
-		assertEquals("101.80",drv.getTenTypes_Text_Float().getAttribute("value"));
+		assertEquals("101,80",drv.getTenTypes_Text_Float().getAttribute("value"));
 	}
 	@Test
 	public void TenTypes_Double(){
 		drv.getAutoFill_button_101().click();
-		assertEquals("101.800",drv.getTenTypes_Text_Double().getAttribute("value"));
+		assertEquals("101,800",drv.getTenTypes_Text_Double().getAttribute("value"));
 	}
 	@Test
 	public void TenTypes_BigInteger(){
@@ -79,7 +79,7 @@ public class NumberConversionInVietnamese extends PMR02698000852_MainClass{
 	@Test
 	public void TenTypes_BigDecimal(){
 		drv.getAutoFill_button_101().click();
-		assertEquals("101.800",drv.getTenTypes_Text_BigDecimal().getAttribute("value"));
+		assertEquals("101,800",drv.getTenTypes_Text_BigDecimal().getAttribute("value"));
 	}
 	
 	@Test
@@ -88,64 +88,82 @@ public class NumberConversionInVietnamese extends PMR02698000852_MainClass{
 		AllWidgetsProjectMain_Class.waitUntilLoadElement(20, "Text_submit_Result_link");
 
 	}
-	/*
+	/**===================assert results===================**/
+	
+	//String
 	@Test
-	public void TenTypes_result_String(){
-		drv.getAutoFill_button_101().click();
-		assertEquals("101.8",drv.getTenTypes_Text_String().getAttribute("value"));
-	}
-	@Test
-	public void TenTypes_result_Currency(){
-		drv.getAutoFill_button_101().click();
-		assertEquals("US$ 101.80",drv.getTenTypes_Text_Currency().getAttribute("value"));
-	}
-	@Test
-	public void TenTypes_result_Number(){
-		drv.getAutoFill_button_101().click();
-		assertEquals("101.8",drv.getTenTypes_Text_Number().getAttribute("value"));
-	}
-	@Test
-	public void TenTypes_result_Short(){
-		drv.getAutoFill_button_101().click();
-		assertEquals("102",drv.getTenTypes_Text_Short().getAttribute("value"));
-	}
-	@Test
-	public void TenTypes_result_Integer(){
-		drv.getAutoFill_button_101().click();
-		assertEquals("102",drv.getTenTypes_Text_Integer().getAttribute("value"));
-	}
-	@Test
-	public void TenTypes_result_Long(){
-		drv.getAutoFill_button_101().click();
-		assertEquals("102",drv.getTenTypes_Text_Long().getAttribute("value"));
-	}
-	@Test
-	public void TenTypes_result_Float(){
-		drv.getAutoFill_button_101().click();
-		assertEquals("101.80",drv.getTenTypes_Text_Float().getAttribute("value"));
-	}
-	@Test
-	public void TenTypes_result_Double(){
-		drv.getAutoFill_button_101().click();
-		assertEquals("101.800",drv.getTenTypes_Text_Double().getAttribute("value"));
-	}
-	@Test
-	public void TenTypes_result_BigInteger(){
-		drv.getAutoFill_button_101().click();
-		assertEquals("102",drv.getTenTypes_Text_BigInteger().getAttribute("value"));
-	}
-	@Test
-	public void TenTypes_result_BigDecimal(){
-		drv.getAutoFill_button_101().click();
-		assertEquals("101.800",drv.getTenTypes_Text_BigDecimal().getAttribute("value"));
+	public void TenTypes_result_Text_String(){
+		assertEquals("101.8",drv.getTenTypes_Text_Result_String().getAttribute("value"));
+		assertEquals("101.8",drv.getTenTypes_Lable_Result_String().getText());
 	}
 	
-	*/
+	//Currency
+	@Test
+	public void TenTypes_result_Text_Currency(){
+		assertEquals("102 VND",drv.getTenTypes_Text_Result_Currency().getAttribute("value"));
+		assertEquals("VND102,00",drv.getTenTypes_Lable_Result_Currency().getText());
+	}
+	
+	//Number
+	@Test
+	public void TenTypes_result_Text_Number(){
+		assertEquals("102",drv.getTenTypes_Text_Result_Number().getAttribute("value"));
+		assertEquals("102",drv.getTenTypes_Lable_Result_Number().getText());
+	}
+	
+	//Short
+	@Test
+	public void TenTypes_result_Text_Short(){
+		assertEquals("102",drv.getTenTypes_Text_Result_Short().getAttribute("value"));
+		assertEquals("102",drv.getTenTypes_Lable_Result_Short().getText());
+	}
+	
+	//Integer
+	@Test
+	public void TenTypes_result_Integer(){
+		assertEquals("102",drv.getTenTypes_Text_Result_Integer().getAttribute("value"));
+		assertEquals("102",drv.getTenTypes_Lable_Result_Integer().getText());
+	}
+	
+	//Long
+	@Test
+	public void TenTypes_result_Long(){
+		assertEquals("102",drv.getTenTypes_Text_Result_Long().getAttribute("value"));
+		assertEquals("102",drv.getTenTypes_Lable_Result_Long().getText());
+	}
+	
+	//Float
+	@Test
+	public void TenTypes_result_Float(){
+		assertEquals("101,80",drv.getTenTypes_Text_Result_Float().getAttribute("value"));
+		assertEquals("101,80",drv.getTenTypes_Lable_Result_Float().getText());
+	}
+	
+	//Double
+	@Test
+	public void TenTypes_result_Double(){
+		assertEquals("101,800",drv.getTenTypes_Text_Result_Double().getAttribute("value"));
+		assertEquals("101,800",drv.getTenTypes_Lable_Result_Double().getText());
+	}
+	
+	//BigInteger
+	@Test
+	public void TenTypes_result_BigInteger(){
+		assertEquals("102",drv.getTenTypes_Text_Result_BigInteger().getAttribute("value"));
+		assertEquals("102",drv.getTenTypes_Lable_Result_BigInteger().getText());
+	}
+	
+	//BigDecimal
+	@Test
+	public void TenTypes_result_BigDecimal(){
+		assertEquals("101,800",drv.getTenTypes_Text_Result_BigDecimal().getAttribute("value"));
+		assertEquals("101,800",drv.getTenTypes_Lable_Result_BigDecimal().getText());
+	}
 	
 	
 	 @AfterClass
 	  public static void tearDown() throws Exception {
-		Tools.snapshot((TakesScreenshot)driver, InternetBankTestWebMain_Class.InternetBankTestWeb,NumberConversionInVietnamese.class);
+		Tools.snapshot((TakesScreenshot)driver, InternetBankTestWebMain_Class.InternetBankTestWeb,NumberConversionInVietnamese101.class);
 		driver.quit();
 //		driver.navigate().refresh();
 	    String verificationErrorString = verificationErrors.toString();
--- BTTAutomation/src/com/ibm/btt/internetbanktestweb/fvt/pmr/p02698000852/PMR02698000852_PageObject.java
+++ BTTAutomation/src/com/ibm/btt/internetbanktestweb/fvt/pmr/p02698000852/PMR02698000852_PageObject.java
@@ -19,13 +19,12 @@ public class PMR02698000852_PageObject {
 		PMR.click();
 	}
 	
-	@FindBy(id="index_link56")
+	@FindBy(id="index_link55")
 	private WebElement PMR_link;
 		
 	public void PMR_link(){
 		PMR_link.click();
 	}
-	
 	
 	@FindBy(id="VI_number_test_link")
 	private WebElement VI_number_test_link;
@@ -33,12 +32,53 @@ public class PMR02698000852_PageObject {
 	public void VI_number_test_link(){
 		VI_number_test_link.click();
 	}
-	
+
+	//fill  101.8
 	@FindBy(id="Text_submit_button01_label")
 	private WebElement AutoFill_button_101;
 
 	public WebElement getAutoFill_button_101() {
 		return AutoFill_button_101;
+	}
+
+	//fill 112.0
+	@FindBy(id="Text_submit_button04_label")
+	private WebElement AutoFill_button_112;
+
+	public WebElement getAutoFill_button_112() {
+		return AutoFill_button_112;
+	}
+	
+	//fill 1208.32
+	@FindBy(id="Text_submit_button03_copy_label")
+	private WebElement AutoFill_button_1208;
+
+	public WebElement getAutoFill_button_1208() {
+		return AutoFill_button_1208;
+	}
+	
+	//fill 1234.00
+	@FindBy(id="Text_submit_button03_copy_copy_label")
+	private WebElement AutoFill_button_1234;
+
+	public WebElement getAutoFill_button_1234() {
+		return AutoFill_button_1234;
+	}
+	
+	//fill 123456789.00
+	@FindBy(id="Text_submit_button03_label")
+	private WebElement AutoFill_button_123456789;
+
+	public WebElement getAutoFill_button_123456789() {
+		return AutoFill_button_123456789;
+	}
+	
+	//fill 123456789.123
+	@FindBy(id="Text_submit_button02_label")
+	private WebElement AutoFill_button_1_9;
+
+	public WebElement getAutoFill_button_1_9() {
+		return AutoFill_button_1_9;
 	}
 	
 	/*
@@ -142,6 +182,167 @@ public class PMR02698000852_PageObject {
 	
 	
 //###############################Result Page##################################	
+	//String-text
+	@FindBy(id="Text_submit_Result_text")
+	private WebElement Text_submit_Result_text;
+
+	public WebElement getTenTypes_Text_Result_String() {
+		return Text_submit_Result_text;
+	}
+	//String-label
+	@FindBy(id="Text_submit_Result_label07")
+	private WebElement Text_submit_Result_label07;
+	
+	public WebElement getTenTypes_Lable_Result_String() {
+		return Text_submit_Result_label07;
+	}
+
+	
+	//Currency-text
+	@FindBy(id="Text_submit_Result_text02")
+	private WebElement Text_submit_Result_text02;
+
+	public WebElement getTenTypes_Text_Result_Currency() {
+		return Text_submit_Result_text02;
+	}
+	//Currency-label
+	@FindBy(id="Text_submit_Result_label10_copy")
+	private WebElement Text_submit_Result_label10_copy;
+	
+	public WebElement getTenTypes_Lable_Result_Currency() {
+		return Text_submit_Result_label10_copy;
+	}
+
+	
+	//Number-text
+	@FindBy(id="Text_submit_Result_text03")
+	private WebElement Text_submit_Result_text03;
+	
+	public WebElement getTenTypes_Text_Result_Number() {
+		return Text_submit_Result_text03;
+	}
+	//Number-label
+	@FindBy(id="Text_submit_Result_label10_copy01")
+	private WebElement Text_submit_Result_label10_copy01;
+	
+	public WebElement getTenTypes_Lable_Result_Number() {
+		return Text_submit_Result_label10_copy01;
+	}
+	
+	
+	//Short-text
+	@FindBy(id="Text_submit_Result_text08")
+	private WebElement Text_submit_Result_text08;
+	
+	public WebElement getTenTypes_Text_Result_Short() {
+		return Text_submit_Result_text08;
+	}
+	//Short-label
+	@FindBy(id="Text_submit_Result_label10_copy06")
+	private WebElement Text_submit_Result_label10_copy06;
+	
+	public WebElement getTenTypes_Lable_Result_Short() {
+		return Text_submit_Result_label10_copy06;
+	}
+	
+	
+	//Integer-text
+	@FindBy(id="Text_submit_Result_text09")
+	private WebElement Text_submit_Result_text09;
+	
+	public WebElement getTenTypes_Text_Result_Integer() {
+		return Text_submit_Result_text09;
+	}
+	//Integer-label
+	@FindBy(id="Text_submit_Result_label10_copy07")
+	private WebElement Text_submit_Result_label10_copy07;
+	
+	public WebElement getTenTypes_Lable_Result_Integer() {
+		return Text_submit_Result_label10_copy07;
+	}
+	
+	
+	//Long-text
+	@FindBy(id="Text_submit_Result_text10")
+	private WebElement Text_submit_Result_text10;
+	
+	public WebElement getTenTypes_Text_Result_Long() {
+		return Text_submit_Result_text10;
+	}
+	//Long-label
+	@FindBy(id="Text_submit_Result_label10_copy08")
+	private WebElement Text_submit_Result_label10_copy08;
+	
+	public WebElement getTenTypes_Lable_Result_Long() {
+		return Text_submit_Result_label10_copy08;
+	}
+	
+	
+	//Float-text
+	@FindBy(id="Text_submit_Result_text11")
+	private WebElement Text_submit_Result_text11;
+	
+	public WebElement getTenTypes_Text_Result_Float() {
+		return Text_submit_Result_text11;
+	}
+	//Float-label
+	@FindBy(id="Text_submit_Result_label10_copy09")
+	private WebElement Text_submit_Result_label10_copy09;
+	
+	public WebElement getTenTypes_Lable_Result_Float() {
+		return Text_submit_Result_label10_copy09;
+	}
+	
+	
+	//Double-text
+	@FindBy(id="Text_submit_Result_text12")
+	private WebElement Text_submit_Result_text12;
+	
+	public WebElement getTenTypes_Text_Result_Double() {
+		return Text_submit_Result_text12;
+	}
+	//Double-label
+	@FindBy(id="Text_submit_Result_label10_copy10")
+	private WebElement Text_submit_Result_label10_copy10;
+	
+	public WebElement getTenTypes_Lable_Result_Double() {
+		return Text_submit_Result_label10_copy10;
+	}
+	
+	
+	//BigInteger-text
+	@FindBy(id="Text_submit_Result_text13")
+	private WebElement Text_submit_Result_text13;
+	
+	public WebElement getTenTypes_Text_Result_BigInteger() {
+		return Text_submit_Result_text13;
+	}
+	//BigInteger-label
+	@FindBy(id="Text_submit_Result_label10_copy11")
+	private WebElement Text_submit_Result_label10_copy11;
+	
+	public WebElement getTenTypes_Lable_Result_BigInteger() {
+		return Text_submit_Result_label10_copy11;
+	}
+		
+	
+	//BigDecimal-text
+	@FindBy(id="Text_submit_Result_text14")
+	private WebElement Text_submit_Result_text14;
+	
+	public WebElement getTenTypes_Text_Result_BigDecimal() {
+		return Text_submit_Result_text14;
+	}
+	//BigDecimal-label
+	@FindBy(id="Text_submit_Result_label10_copy12")
+	private WebElement Text_submit_Result_label10_copy12;
+	
+	public WebElement getTenTypes_Lable_Result_BigDecimal() {
+		return Text_submit_Result_label10_copy12;
+	}
+
+	
+	
 	//return
 	@FindBy(id="Text_submit_Result_link")
 	private WebElement ReturnLink;
