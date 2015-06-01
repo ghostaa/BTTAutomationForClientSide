d67864081132947d746eabd60c6d2f3298b83518
sj <sj@0d26679e-a245-7d45-8790-df05fb349fae>
2015-06-01 07:49:39 +0000

    vi-text data auto test script
    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@217 0d26679e-a245-7d45-8790-df05fb349fae

--- BTTAutomation/src/com/ibm/btt/testsuite/InternetBankTestWebAllTestsSuite.java
+++ BTTAutomation/src/com/ibm/btt/testsuite/InternetBankTestWebAllTestsSuite.java
@@ -4,11 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.ibm.btt.internetbanktestweb.fvt.pmr.p02698000852.NumberConversionInVietnamese;
+import com.ibm.btt.internetbanktestweb.fvt.pmr.p02698000852.textLabel.NumberConversionInVietnamese101;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	NumberConversionInVietnamese.class
+	NumberConversionInVietnamese101.class
 	
 })
 public class InternetBankTestWebAllTestsSuite {
