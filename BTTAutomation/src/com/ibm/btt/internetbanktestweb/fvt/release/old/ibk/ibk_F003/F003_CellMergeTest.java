0d1cd4f54cb4d9201dfb18d6fed62850c6486b55
sxl <sxl@0d26679e-a245-7d45-8790-df05fb349fae>
2015-07-15 08:23:26 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@261 0d26679e-a245-7d45-8790-df05fb349fae

--- BTTAutomation/src/com/ibm/btt/internetbanktestweb/fvt/release/old/ibk/ibk_F003/F003_CellMergeTest.java
+++ BTTAutomation/src/com/ibm/btt/internetbanktestweb/fvt/release/old/ibk/ibk_F003/F003_CellMergeTest.java
@@ -9,17 +9,18 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 
 import com.ibm.btt.util.InternetBankTestWebMain_Class;
+import com.ibm.btt.util.TableTestUtil;
 
 public class F003_CellMergeTest extends InternetBankTestWebMain_Class {
 
 	private static F003_Index_PageObject drv;
 	private static F003_Cell_PageObject cell_drv;
+	private static F003_Result_BodyCell_PageObject result_drv;
 	private static StringBuffer verificationErrors = new StringBuffer();
 	
 	@BeforeClass
@@ -30,6 +31,8 @@ public class F003_CellMergeTest extends InternetBankTestWebMain_Class {
 		cell_drv = PageFactory.initElements(driver,
 				F003_Cell_PageObject.class);
 		// driver.navigate().to(Main_Class.baseUrl);
+		result_drv = PageFactory.initElements(driver,
+				F003_Result_BodyCell_PageObject.class);
 		drv.Establish();
 		drv.IBK_FVT_tab();
 		drv.F003();
@@ -39,48 +42,58 @@ public class F003_CellMergeTest extends InternetBankTestWebMain_Class {
 	@Test
 	public void testHeader() {
 		// Grab the table's header, the header has one row, and four columns.
-		WebElement tableHeader = cell_drv.getTableHeader();
-		List<WebElement> allHeaderRows = tableHeader.findElements(By.tagName("tr"));
+		WebElement table = cell_drv.getTable();
+		List<WebElement> allHeaderRows = TableTestUtil.getTableHeaderRows(table);
 		assertEquals(1, allHeaderRows.size());
-		WebElement headerRow = allHeaderRows.get(0);
-		List<WebElement> headers = headerRow.findElements(By.tagName("th"));
-		assertEquals(4, headers.size());
+		List<WebElement> headers_0 = TableTestUtil.getTableHeaderCellsByHRow(table, 0);
+		assertEquals(4, headers_0.size());
 	}
 	
 	@Test
 	public void testBodyCellMerge(){
-		WebElement tableBody = cell_drv.getTableBody();
+		WebElement table = cell_drv.getTable();
 		// the cell merge is worked as display the specified cells, and hide others.
-		List<WebElement> bTables = tableBody.findElements(By.tagName("table"));
-		assertEquals(10, bTables.size());
-		WebElement bTable9 = bTables.get(9);
-		WebElement bTable9Tr = bTable9.findElement(By.tagName("tr"));
-		List<WebElement> bTable9Tr_Tds = bTable9Tr.findElements(By.tagName("td"));
-		assertEquals(4, bTable9Tr_Tds.size());
-		assertEquals("none", bTable9Tr_Tds.get(0).getCssValue("display"));
-		assertEquals("table-cell", bTable9Tr_Tds.get(1).getCssValue("display"));
-		assertEquals("2", bTable9Tr_Tds.get(1).getAttribute("colspan"));
-		assertEquals("table-cell", bTable9Tr_Tds.get(2).getCssValue("display"));
-		assertEquals("2", bTable9Tr_Tds.get(2).getAttribute("colspan"));
-		assertEquals("none", bTable9Tr_Tds.get(3).getCssValue("display"));
+		List<WebElement> rows = TableTestUtil.getTableBodyRows(table);
+		assertEquals(10, rows.size());
+		List<WebElement> cells_9 = TableTestUtil.getTableBodyCellsByBRow(table, 9);
+		assertEquals(4, cells_9.size());
+		assertEquals("none", cells_9.get(0).getCssValue("display"));
+		assertEquals("table-cell", cells_9.get(1).getCssValue("display"));
+		assertEquals("2", cells_9.get(1).getAttribute("colspan"));
+		assertEquals("table-cell", cells_9.get(2).getCssValue("display"));
+		assertEquals("2", cells_9.get(2).getAttribute("colspan"));
+		assertEquals("none", cells_9.get(3).getCssValue("display"));
 		
 	}
 	
 	@Test
 	public void testEditSubmit(){
-		WebElement tableBody = cell_drv.getTableBody();
-		List<WebElement> bTables = tableBody.findElements(By.tagName("table"));
-		WebElement bTable9 = bTables.get(9);
-		List<WebElement> spans = bTable9.findElements(By.tagName("span"));
-		Actions action = new Actions(driver);
-		WebElement span = spans.get(1);
-		action.moveToElement(span).doubleClick().build().perform();
+		WebElement table = cell_drv.getTable();
+		WebElement row9 = TableTestUtil.getTableBodyRows(table).get(9);
+		WebElement cell_9_1 = TableTestUtil.getTableBodyCellByBRowBCol(table, 9, 1);
+		Actions action1 = new Actions(driver);
+		Actions action2 = new Actions(driver);
+//		action1.moveToElement(td_9_1).doubleClick().click().sendKeys("totalprice").build().perform();
+		action1.moveToElement(cell_9_1).doubleClick().click().build().perform();
+		WebElement inputCell = row9.findElement(By.id("com_ibm_btt_dijit_StringTextBox_1"));
+		inputCell.clear();
+		inputCell.sendKeys("Total_Price");
+		WebElement F003_B_button = cell_drv.getBButton();
+		action2.moveToElement(F003_B_button).click().build().perform();
+		WebElement res_sel_table = result_drv.getMultiSelectTable();
+		int sel_rowNum =  TableTestUtil.getTableBodyRows(res_sel_table).size() - 1;
+		WebElement sel_t_cell = TableTestUtil.getTableBodyCellByBRowBCol(res_sel_table, sel_rowNum, 1);
+		assertEquals("Total_Price", sel_t_cell.getText());
+		WebElement res_upd_table = result_drv.getUpdatedTable();
+		int upd_rowNum = TableTestUtil.getTableBodyRows(res_upd_table).size() - 1;
+		WebElement upd_t_cell = TableTestUtil.getTableBodyCellByBRowBCol(res_upd_table, upd_rowNum, 5);
+		assertEquals("Total_Price", upd_t_cell.getText());
 	}
 	
 	@AfterClass
 	public static void tearDown() throws Exception {
 //		Tools.snapshot((TakesScreenshot)driver, InternetBankTestWebMain_Class.InternetBankTestWeb,F003_CellMergeTest.class);
-//		driver.quit();
+		driver.quit();
 		// driver.navigate().refresh();
 	    String verificationErrorString = verificationErrors.toString();
 	    if (!"".equals(verificationErrorString)) {
--- BTTAutomation/src/com/ibm/btt/internetbanktestweb/fvt/release/old/ibk/ibk_F003/F003_Cell_PageObject.java
+++ BTTAutomation/src/com/ibm/btt/internetbanktestweb/fvt/release/old/ibk/ibk_F003/F003_Cell_PageObject.java
@@ -1,6 +1,5 @@
 package com.ibm.btt.internetbanktestweb.fvt.release.old.ibk.ibk_F003;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -10,16 +9,15 @@ public class F003_Cell_PageObject{
 	//###############################F003 Cell Merge Page##################################
 	@FindBy(id="F003_BodyCell_table")
 	private WebElement F003CellTable;
+	@FindBy(id="F003_BodyCell_button")
+	private WebElement F003_B_button;
 	
-	public WebElement getTableHeader(){
-		WebElement tableHeader = F003CellTable.findElement(By
-				.className("dojoxGridMasterHeader"));
-		return tableHeader;
+	public WebElement getTable(){
+		return F003CellTable;
 	}
 	
-	public WebElement getTableBody(){
-		WebElement tableBody = F003CellTable.findElement(By
-				.className("dojoxGridMasterView"));
-		return tableBody;
+	
+	public WebElement getBButton(){
+		return F003_B_button;
 	}
 }
