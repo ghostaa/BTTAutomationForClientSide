15da5b6f4bed48198a376349c1c882a387042582
sxl <sxl@0d26679e-a245-7d45-8790-df05fb349fae>
2015-07-16 09:29:20 +0000

    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@270 0d26679e-a245-7d45-8790-df05fb349fae

--- BTTAutomation/src/com/ibm/btt/internetbanktestweb/fvt/release/old/ibk/ibk_F003/F003_BothMergeTest.java
+++ BTTAutomation/src/com/ibm/btt/internetbanktestweb/fvt/release/old/ibk/ibk_F003/F003_BothMergeTest.java
@@ -1,7 +1,6 @@
 package com.ibm.btt.internetbanktestweb.fvt.release.old.ibk.ibk_F003;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.List;
