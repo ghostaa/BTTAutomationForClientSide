67138fe024bf0a0efd5c8cb3d89e88a65e463cbd
sj <sj@0d26679e-a245-7d45-8790-df05fb349fae>
2016-03-01 09:03:23 +0000

    textarea has finished
    git-svn-id: file:///c/Repositories/BTTAutomationForClientSide@306 0d26679e-a245-7d45-8790-df05fb349fae

--- BTTAutomation/src/com/ibm/btt/allwidgets/TextArea/TextAreaSuite.java
+++ BTTAutomation/src/com/ibm/btt/allwidgets/TextArea/TextAreaSuite.java
@@ -4,13 +4,13 @@ import org.junit.runners.Suite;
   
 @RunWith(Suite.class)  
 @Suite.SuiteClasses({    
-/*	TextArea_Properties.class,    
+	TextArea_Properties.class,    
 	TextArea_event.class,
 	TextArea_condition.class,
 	TextArea_action.class,
 	TextArea_CSS.class,
 	TextArea_appearance1.class,
-	TextArea_appearance2.class,*/
+	TextArea_appearance2.class,
 	TextArea_Submit.class,
 	TextArea_SubmitWithNoData.class,
 	TextArea_SubmitWithoutValidation.class,
